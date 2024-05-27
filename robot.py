bdf04b6ee4444996526290e3d4811e1fe2251c9a
Umesh Ranasinghe <umesh.ranasinghe@gmail.com>
2024-05-27 23:50:19 +1000

    introduce functions for moving

--- robot.py
+++ robot.py
@@ -1,5 +1,24 @@
 import re
 
+def move_forward():
+  print(f"Forward")
+
+def move_backward():
+  print(f"Backward")
+
+def turn_left():
+  print(f"Turning left")
+
+def turn_right():
+  print(f"Turning right")
+
+actions = {
+    'F': move_forward,
+    'B': move_backward,
+    'L': turn_left,
+    'R': turn_right,
+}
+
 def validate_and_parse_commands(user_commands_str):
   single_command_pattern = r'[FBLR]\d+'
   full_command_pattern = '^{0}(,{0})*$'.format(single_command_pattern)
@@ -11,6 +30,14 @@ def validate_and_parse_commands(user_commands_str):
     return parsed_commands
   else:
     return None
+
+def execute_commands(commands):
+  for command in commands:
+    print(command)
+    action = command[0]
+    execution_count = command[1]
+    for i in range(execution_count):
+      actions[action]()
 
 def initialize():
   global position, facing_direction
@@ -24,6 +51,7 @@ if __name__ == "__main__":
   if validated_commands:
     print('ready to move. plan is {0}'.format(validated_commands))
     initialize()
+    execute_commands(valid_commands)
   else:
     print('Invalid comamand : {}'.format(user_commands_str))
   print(4)
