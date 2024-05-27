3a6b70dd6a5024946b8e19db90d5391adf1fb90d
Umesh Ranasinghe <umesh.ranasinghe@gmail.com>
2024-05-28 00:53:35 +1000

    add argument parser to receive user input

--- robot.py
+++ robot.py
@@ -1,4 +1,4 @@
-import re
+import re, argparse
 
 def move_forward():
   global position
@@ -32,11 +32,11 @@ actions = {
 def validate_and_parse_commands(user_commands_str):
   single_command_pattern = r'[FBLR]\d+'
   full_command_pattern = '^{0}(,{0})*$'.format(single_command_pattern)
-  valid_command = re.fullmatch(full_command_pattern, user_commands_str)
+  valid_command = re.fullmatch(full_command_pattern, user_commands_str)        #  match the whole user command with the accepted pattern
     
   if valid_command:
     single_commands = re.findall(r'([FBLR])(\d+)', user_commands_str)
-    parsed_commands = [(letter, int(number)) for letter, number in single_commands]
+    parsed_commands = [(action_letter, int(action_count)) for action_letter, action_count in single_commands]        #  extract action and the number of times it should be executed
     return parsed_commands
   else:
     return None
@@ -48,7 +48,7 @@ def execute_commands(commands):
     execution_count = command[1]
     
     if action in ['L','R']:
-      execution_count = execution_count%4
+      execution_count = execution_count%4        #  avoid full circle turns
     for i in range(execution_count):
       actions[action]()
 
@@ -59,10 +59,31 @@ def initialize():
 
 def find_min_distance_to_return():
     return abs(position[0]) + abs(position[1])
+
+def parse_input_arguments():
+  parser = argparse.ArgumentParser(
+    description="""This program receives a string of commands 
+    and will output the robot's distance from it's starting point.  
+    This distance will be the minimum amount of units the robot 
+    will need to traverse in order to get back to it's starting point.
+    
+    Example command string: F1,R1,B2,L1,B3"""
+  )
+  parser.add_argument(
+    'command_string', type=str, 
+    help='Navigation commands to robot in comma seperated format'
+  )
+  parser.add_argument(
+    '-d', '--debug', action='store_true', 
+    help='Enable debug output')
+  args = parser.parse_args()
+  global debug_mode
+  debug_mode = args.debug
+  return args.command_string
   
 if __name__ == "__main__":
-#  user_commands_str = input("Enter instructions for robot :")
-  user_commands_str = "F1,R5,B2,L9,B3"
+  user_commands_str = parse_input_arguments()
+  #user_commands_str = "F1,R5,B2,L9,B3"
   validated_commands = validate_and_parse_commands(user_commands_str)
   
   if validated_commands:
