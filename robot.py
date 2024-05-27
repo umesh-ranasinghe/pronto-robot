ded524800d4348efacb46602a63f5c46e58e2c28
Umesh Ranasinghe <umesh.ranasinghe@gmail.com>
2024-05-27 18:51:24 +1000

    initial commit
    initial commit

---
+++ robot.py
@@ -0,0 +1,8 @@
+def initialize():
+  global position, facing_direction
+  position = [0,0]
+  facing_direction = [0,1]
+
+if __name__ == "__main__":
+  initialize()
+  print('current position : {0}'.format(position))
