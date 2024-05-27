3b6952897ce99b893c5ea07bf46184a9969a2198
Umesh Ranasinghe <umesh.ranasinghe@gmail.com>
2024-05-28 02:16:28 +1000

    enhance formatting

--- test_robot.py
+++ test_robot.py
@@ -1,8 +1,10 @@
 import subprocess
 
+
 def test_robot_valid_command():
     result = subprocess.run(["python", "robot.py", "F1,R1,B2,L1,B3"], capture_output=True, text=True)
     assert result.stdout.strip() == "4"
+
 
 def test_robot_invalid_command():
     result = subprocess.run(["python", "robot.py", "F1L,R1"], capture_output=True, text=True)
