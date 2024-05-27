d8cbcfd6211dcdec628f253874854684a5555c5a
Umesh Ranasinghe <umesh.ranasinghe@gmail.com>
2024-05-28 01:59:19 +1000

    set debug mode variable for test case

--- test_robot.py
+++ test_robot.py
@@ -6,5 +6,5 @@ def test_robot():
     assert result.stdout.strip() == "4"
 
 def test_validate_and_parse_commands():
-    debug_mode = False
+    globals()['debug_mode'] = False
     assert validate_and_parse_commands("F1,R1,B2,L1,B3") == [('F', 1), ('R', 5), ('B', 2), ('L', 9), ('B', 3)]
